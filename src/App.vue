170cb7aab515e4256d4ee9e08928b4b927c273b1
steekam <waynewanyee@gmail.com>
2021-04-12 22:12:09 +0300

    wip: desktop version styling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
- <main class="grid h-screen grid-cols-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-   <div class="flex flex-col items-center justify-center gap-10">
-     <h1>Grab a friend and play the Tic Tac Toe game.</h1>
-     <p>Relive the childhood memories.</p>
+ <main class="grid h-screen max-w-5xl grid-cols-2 mx-auto sm:px-6 lg:px-8">
+   <div class="flex flex-col justify-center gap-6">
+     <h1 class="text-5xl font-semibold text-gray-800">Wanna play the Tic-tac-toe game?</h1>
+     <p class="text-2xl font-medium text-gray-700">Grab a friend and start playing.</p>
+
+     <div>
+      <button class="px-3 py-2 text-lg font-semibold text-green-800 capitalize border rounded-full shadow-sm ring-2 ring-green-800">
+        Start new game
+      </button>
+     </div>
    </div>
 
    <div class="flex items-center justify-center">
-     <div class="px-10 py-6 bg-white shadow-lg">
-       <header></header>
+     <div class="px-10 py-6 space-y-6 bg-white rounded-md shadow-lg">
+       <header class="flex justify-between">
+         <div class="flex flex-col gap-2 text-blue-800">
+           <div>X</div>
+           <div class="text-sm">
+             <span>4</span> wins
+           </div>
+         </div>
+
+         <div class="flex flex-col gap-2 text-green-800">
+           <div>O</div>
+           <div class="text-sm">
+             <span>4</span> wins
+           </div>
+         </div>
+
+         <div class="flex flex-col gap-2 text-gray-600">
+           <div>O</div>
+           <div class="text-sm">
+             <span>2</span> draws
+           </div>
+         </div>
+       </header>
        
        <!-- Booard grid -->
        <div class="grid grid-cols-3 grid-rows-3 board">
@@ -15,10 +42,10 @@
        </div>
 
        <!-- Active player indicator  -->
-       <div></div>
-      
-      <!-- Game options -->
-       <footer></footer>
+       <div class="grid w-20 grid-cols-2 mx-auto overflow-hidden font-bold border-2 rounded-full">
+         <div class="flex justify-center p-2 rounded-full cursor-pointer">X</div>
+         <div class="flex justify-center p-2 text-white bg-blue-700 rounded-full cursor-pointer">O</div>
+       </div>
      </div>
    </div>
  </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: colors.emerald,
+        blue: colors.cyan,
+      }
+    },
   },
   variants: {
     extend: {},
